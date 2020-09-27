2306af51571daac9e3db52efb61564a5b5833ab8
Tony S <sanjayatony@gmail.com>
2020-09-27 13:02:35 +0700

    create dirs

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const path = require("path");
 const execa = require("execa");
 const listr = require("listr");
 const inquirer = require("inquirer");
+const { exit } = require("process");
 
 const dependencies = [
   "autoprefixer",
@@ -15,3 +16,29 @@ const dependencies = [
   "postcss-nested",
   "tailwindcss",
 ];
+
+const root_dir = process.argv.slice(2);
+
+// The tasks
+const tasks = new listr([
+  {
+    title: "Create directory structure",
+    task: (context) => {
+      const tree = ["dist/assets/css", "dist/assets/img"];
+
+      if (fs.existsSync(root_dir)) {
+        throw new Error(`Project directory ${root_dir} already exists`);
+      }
+      fs.mkdirSync("./" + root_dir);
+
+      tree.forEach((branch) => {
+        const dir = path.join("./" + root_dir, branch);
+        fs.mkdirSync(dir, { recursive: true }, (err) => {
+          if (err) throw new Error(`Cannot create directory ${dir}: ${err}`);
+        });
+      });
+    },
+  },
+]);
+
+tasks.run().catch((err) => console.log(err));
