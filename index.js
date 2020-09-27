61ed4c4df2edb9cd7a10e8868854395f307ef4df
Tony S <sanjayatony@gmail.com>
2020-09-27 14:03:14 +0700

    fix dir name

--- index.js
+++ index.js
@@ -17,27 +17,31 @@ const dependencies = [
   "tailwindcss",
 ];
 
-const root_dir = process.argv.slice(2);
+const root_dir = "./" + process.argv.slice(2);
 
 // The tasks
 const tasks = new listr([
   {
     title: "Create directory structure",
-    task: (context) => {
+    task: () => {
       const tree = ["dist/assets/css", "dist/assets/img"];
 
       if (fs.existsSync(root_dir)) {
         throw new Error(`Project directory ${root_dir} already exists`);
       }
-      fs.mkdirSync("./" + root_dir);
+      fs.mkdirSync(root_dir);
 
       tree.forEach((branch) => {
-        const dir = path.join("./" + root_dir, branch);
+        const dir = path.join(root_dir, branch);
         fs.mkdirSync(dir, { recursive: true }, (err) => {
           if (err) throw new Error(`Cannot create directory ${dir}: ${err}`);
         });
       });
     },
+  },
+  {
+    title: "Create project files",
+    task: () => {},
   },
 ]);
 
