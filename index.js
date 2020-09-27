0d27c680b2bb929e33b2bd7f8d57aac922ebee76
Tony S <sanjayatony@gmail.com>
2020-09-27 17:03:11 +0700

    add postcss + concurrently

--- index.js
+++ index.js
@@ -12,9 +12,12 @@ const { exit } = require("process");
 const dependencies = [
   "autoprefixer",
   "cssnano",
+  "postcss",
   "postcss-cli",
   "postcss-nested",
   "tailwindcss",
+  "live-server",
+  "concurrently",
 ];
 
 const root_dir = "./" + process.argv.slice(2);
@@ -25,7 +28,7 @@ const tasks = new listr([
   {
     title: "Create directory structure",
     task: () => {
-      const tree = ["dist/assets/css", "dist/assets/img"];
+      const tree = ["public/assets/css", "public/assets/img"];
 
       if (fs.existsSync(root_dir)) {
         throw new Error(`Project directory ${root_dir} already exists`);
@@ -45,33 +48,20 @@ const tasks = new listr([
     title: "Create project files",
     task: () => {
       return new listr([
-        // 2a. index.html
+        // 2a. tailwind.config.js
         {
-          title: "Create index.html",
-          task: async() =>{
-            const source = path.join(__dirname, "files", "index.html");
-            const destination = path.join(root_dir, "dist", "tailwind.css);
-
-            fs.copyFile(source, destination, (err) => {
-              if (err)
-                throw new Error(`Cannot create index.html: ${err}`);
-            });
-          }
-        },
-        // 2b. tailwind.css
-        {
-          title: "Create default tailwind.css",
+          title: "Create default tailwind.config.js",
           task: async () => {
-            const source = path.join(__dirname, "files", "tailwind.css");
-            const destination = path.join(root_dir, "tailwind.css);
+            const source = path.join(__dirname, "files", "tailwind.config.js");
+            const destination = path.join(root_dir, "tailwind.config.js");
 
             fs.copyFile(source, destination, (err) => {
               if (err)
-                throw new Error(`Cannot create tailwind.css: ${err}`);
+                throw new Error(`Cannot create tailwind.config.js: ${err}`);
             });
           },
         },
-        // 2c. postcss
+        // 2b. postcss
         {
           title: "Create PostCSS configuration",
           task: async () => {
@@ -84,12 +74,36 @@ const tasks = new listr([
             });
           },
         },
-        // 2d. gitignore
+        // 2c. tailwind.css
+        {
+          title: "Create default tailwind.css",
+          task: async () => {
+            const source = path.join(__dirname, "files", "tailwind.css");
+            const destination = path.join(root_dir, "tailwind.css");
+
+            fs.copyFile(source, destination, (err) => {
+              if (err) throw new Error(`Cannot create tailwind.css: ${err}`);
+            });
+          },
+        },
+        // 2d. index.html
+        {
+          title: "Create index.html",
+          task: async () => {
+            const source = path.join(__dirname, "files", "index.html");
+            const destination = path.join(root_dir, "public", "index.html");
+
+            fs.copyFile(source, destination, (err) => {
+              if (err) throw new Error(`Cannot create index.html: ${err}`);
+            });
+          },
+        },
+        // 2e. gitignore
         {
           title: "Create gitignore file",
           task: async (context) => {
             const source = path.join(__dirname, "files", "_gitignore");
-            const destination = path.join(root, ".gitignore");
+            const destination = path.join(root_dir, ".gitignore");
 
             fs.copyFile(source, destination, (err) => {
               if (err) throw new Error(`Cannot create gitignore file: ${err}`);
@@ -102,7 +116,7 @@ const tasks = new listr([
   // 3. setup dependencies
   {
     title: "Setup dependencies",
-    task: ()=>{
+    task: () => {
       return new listr([
         // 3a. package.json
         {
@@ -112,28 +126,22 @@ const tasks = new listr([
             const destination = path.join(root_dir, "package.json");
 
             fs.copyFile(source, destination, (err) => {
-              if (err)
-                throw new Error(`Cannot create package.json: ${err}`);
+              if (err) throw new Error(`Cannot create package.json: ${err}`);
             });
           },
-
-
         },
         //3b. Install depedencies
         {
-          title: 'Install dependencies',
+          title: "Install dependencies",
           task: async () => {
-            const params = ['install', '--save-dev'];
-            await execa('npm', [...params, ...dependencies]);
+            const params = ["install", "--save-dev"];
+            process.chdir(root_dir);
+            await execa("npm", [...params, ...dependencies]);
           },
-        }k
-
-      ])
-    }
-
-    }
-
-  }
+        },
+      ]);
+    },
+  },
 ]);
 
 tasks.run().catch((err) => console.log(err));
